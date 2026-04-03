b6f4afad02cd24abb5146e0f37f366e5a2e21ac6
Nguyen Manh Ha <ngmanhha2@gmail.com>
2026-04-04 02:15:32 +0700

    tat kiem tra eslint cho firestore

--- src/firebase.ts
+++ src/firebase.ts
@@ -5,8 +5,8 @@ import firebaseConfig from '../firebase-applet-config.json';
 
 const app = initializeApp(firebaseConfig);
 
-// Thêm dòng này để ép TypeScript bỏ qua kiểm tra lỗi
-// @ts-ignore
+// Thêm dòng dưới đây để cấm ESLint báo lỗi "any"
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const db = (getFirestore as any)(app, firebaseConfig.firestoreDatabaseId);
 
 export const auth = getAuth();
